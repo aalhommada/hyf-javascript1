b07d0caa648d1d461ecdb76be722643175ce277c
neveenatik <nevine.atike@gmail.com>
2018-04-15 15:56:26 +0200

    anitial commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+console.log('Hello world');
+
+let hyf = null;
+const hello = 'Hello world';
+const part = hello.slice(0, 5);
+const upper = hello.toLocaleUpperCase();
+const startsWithHello = hello.startsWith('Hi')
+console.log(part[0], upper);
