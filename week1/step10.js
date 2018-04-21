a55c17618f6d6895273f95732cc5979894952214
neveenatik <nevine.atike@gmail.com>
2018-04-21 04:10:38 +0200

    Modifies step 10 of homework week 1

--- week1/step10.js
+++ week1/step10.js
@@ -3,3 +3,15 @@ console.log("Here is an array with the following elements: " + array)
 console.log("the type of " + array[0] + " is " + typeof array[0]);
 console.log("the type of " + array[1] + " is " + typeof array[1]);
 console.log("the type of " + array[2] + " is " + typeof array[2]);
+let x = 6/0;
+let y = 10/0;
+if (x === y) {
+  console.log("You CAN compare infinities because " + x + " = " + y);
+}
+if (x > y) {
+  console.log("While both results are infinity 6/0 is greater than 10/0");
+}
+if (x < y) {
+  console.log("While both results are infinity 10/0 is greater than 6/0");
+}
+else console.log("Both are infinities but no infinity is greater than the other");
