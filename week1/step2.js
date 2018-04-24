18db116e8eb531714258b2e77b336d5964fc8ec9
neveenatik <nevine.atike@gmail.com>
2018-04-24 04:42:47 +0200

    removing extra spaces from step2 and step4

--- week1/step2.js
+++ week1/step2.js
@@ -1,2 +1,2 @@
 "use strict";
-console.log('I \'m awesome'); // added escape (backspace before the symbol)
+console.log('I\'m awesome'); // added escape (backspace before the symbol)
--- week1/step4.js
+++ week1/step4.js
@@ -3,5 +3,5 @@ let y = "numbers";
 console.log("the value of my string will be: numbers");
 console.log(y);
 y = "letters";
-console.log("the value of my string will be: ", y);
+console.log("the value of my string will be:", y);
 console.log(y);
