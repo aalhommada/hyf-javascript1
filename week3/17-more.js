c65f0e9d40a6ee29d662500907a5a4417782b1d7
neveenatik <nevine.atike@gmail.com>
2018-05-04 22:39:59 +0200

    Modified log message in 17-more.js

--- week3/17-more.js
+++ week3/17-more.js
@@ -1,3 +1,4 @@
+'use strict';
 let bar = 42;
 typeof typeof bar;
-console.log(typeof typeof bar);
+console.log('typeof typeof bar returns ' + typeof typeof bar + ' because typeof bar returns ' + typeof bar + ' and that is a string.');
